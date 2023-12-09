975b430284fd1a000f60e272def7494771e93d92
Hurlu <mr.hurlu@gmail.com>
2023-12-09 15:21:38 +0100

    accept your weaknesses

--- teamspeak/configure_server.sh
+++ teamspeak/configure_server.sh
@@ -4,10 +4,13 @@ DOCKER_IP=$(docker inspect --format '{{ range.NetworkSettings.Networks}}{{.IPAdd
 BASE_COMMAND="http $DOCKER_IP:10080/1"
 API_KEY="?api-key=$(cat api_key.secret)"
 
-GUEST_ID=$($BASE_COMMAND/servergrouplist$API_KEY | jq -r '.body[4].sgid')
-echo "{\"sgid\": \"$GUEST_ID\", \"permsid\":\"i_client_needed_move_power\", \"permvalue\":0, \"permnegated\":0, \"permskip\":0 }" | $BASE_COMMAND/servergroupaddperm$API_KEY
-echo
-exit 0
+
+#Trying to give Guests permission to move anyone among channels, not working yet so doing it manually in ts3-client
+#GUEST_ID=$($BASE_COMMAND/servergrouplist$API_KEY | jq -r '.body[4].sgid')
+#echo "{\"sgid\": \"$GUEST_ID\", \"permsid\":\"i_client_needed_move_power\", \"permvalue\":0, \"permnegated\":0, \"permskip\":0 }" | $BASE_COMMAND/servergroupaddperm$API_KEY
+#echo
+
+#exit 0
 
 
 AFK_ID=$(echo '{"channel_name": "AFK", "channel_flag_permanent":1, "channel_needed_talk_power":999}' | $BASE_COMMAND/channelcreate$API_KEY | jq -r '.body[0].cid')
